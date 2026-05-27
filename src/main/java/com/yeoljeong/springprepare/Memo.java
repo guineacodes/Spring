9c0d805748e3b3d0ea0377a0ebacb0f193a03e88
yeojin-c <orlina2001@naver.com>
2026-05-27 23:45:06 +0900

    get,set

---
+++ src/main/java/com/yeoljeong/springprepare/Memo.java
@@ -0,0 +1,15 @@
+package com.yeoljeong.springprepare;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Memo {
+
+  @Getter
+  @Setter
+  private  String username;
+  private String content;{
+
+  }
+
+}
